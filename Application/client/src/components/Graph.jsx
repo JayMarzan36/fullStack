9f3309bdf333c57019e07aed26df59f2473e936d
JayMarzan36 <jaymarzan36@gmail.com>
2025-04-14 11:01:58 -0600

    Started working on making the graph

--- Application/client/src/components/Graph.jsx
+++ Application/client/src/components/Graph.jsx
@@ -1,6 +1,27 @@
-import * as d3 from "d3"
+import {
+  SimulationNodeDatum,
+  SimulationLinkDatum,
+} from "d3-force";
+
+import { FC, Memo } from "react"
+
+// Following https://medium.com/@qdangdo/visualizing-connections-a-guide-to-react-d3-force-graphs-typescript-74b7af728c90 for making my graph
+// TODO fix
+interface CustomNode extends SimulationNodeDatum {
+        id: String;
+        name: String;
+}
+
+interface CustomLink extends SimulationLinkDatum { 
+    strength: Number
+}
+
 export function Graph() {
-//TODO figure out how to use d3 to represent the notes data for graph
+    //TODO figure out how to use d3 to represent the notes data for graph
+    
+    
+
+
     return (
         <>
             <h1>View Graph</h1>
